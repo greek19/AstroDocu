31200725104afc2467639a91c960afdf7a84b900
Francesco Greco <f.greco@exolab.it>
2022-08-24 17:27:45 +0200

    fix path

--- src/config.ts
+++ src/config.ts
@@ -51,9 +51,9 @@ export const SIDEBAR = {
 	it:[
 		{ text: '', header: true },
 		{ text: 'Chimica', header: true },
-		{ text: 'Introduction', link: 'it/introduction' },
-		{text: 'Colore', link: 'it/colore'},
-		{text: 'Bianco e nero', link: 'it/biancoNero'},
+		{ text: 'Introduction', link: 'AstroDocu/it/introduction' },
+		{text: 'Colore', link: 'AstroDocu/it/colore'},
+		{text: 'Bianco e nero', link: 'AstroDocu/it/biancoNero'},
 
 	]
 };
