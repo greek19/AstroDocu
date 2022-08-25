b18b6f065ab7fe0af48ee637f03b22e91577c14a
Francesco Greco <f.greco@exolab.it>
2022-08-25 11:18:10 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ export default defineConfig({
         // Enable React for the Algolia search component.
         react(),
     ],
-    site: `https://greek19.github.io/`,
+    site: `https://greek19.github.io/AstroDocu`,
     base: "/AstroDocu",
 });
